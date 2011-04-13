7924bf343b77e2dde4642852184c51722fb25e97
Zhihao Yuan <lichray@gmail.com>
2011-04-12 20:00:38 -0500

    hi-fi LPCM audio support; initial a-law/mu-law

--- wavplay.c
+++ wavplay.c
@@ -12,8 +12,16 @@
 #define PCM_NAME "default"
 #define BUF_SIZE 4096
 #define PERIOD   2
+#define eputs(s) (fprintf(stderr, "%s: " s "\n", __func__))
 
 #if !defined(USE_ALSA)
+
+#define WAV_FMT_8      AFMT_U8;
+#define WAV_FMT_16     AFMT_S16_LE;
+#define WAV_FMT_24     AFMT_S24_LE;
+#define WAV_FMT_32     AFMT_S32_LE;
+#define WAV_FMT_A_LAW  AFMT_A_LAW;
+#define WAV_FMT_MU_LAW AFMT_MU_LAW;
 
 static int devfd = -1;
 
@@ -48,6 +56,13 @@ void snd_play(FILE *fp, size_t n) {
 
 #else
 
+#define WAV_FMT_8      SND_PCM_FORMAT_U8;
+#define WAV_FMT_16     SND_PCM_FORMAT_S16;
+#define WAV_FMT_24     SND_PCM_FORMAT_S24;
+#define WAV_FMT_32     SND_PCM_FORMAT_S32;
+#define WAV_FMT_A_LAW  SND_PCM_FORMAT_A_LAW;
+#define WAV_FMT_MU_LAW SND_PCM_FORMAT_MU_LAW;
+
 static snd_pcm_t *pcm = NULL;
 
 int snd_init(void) {
@@ -61,7 +76,7 @@ void snd_set(int format, int nchannels, int framerate) {
 	snd_pcm_hw_params_alloca(&params);
 	snd_pcm_hw_params_any(pcm, params);
 	snd_pcm_hw_params_set_access(pcm, params, SND_PCM_ACCESS_RW_INTERLEAVED);
-	snd_pcm_hw_params_set_format(pcm, params, format / 8);
+	snd_pcm_hw_params_set_format(pcm, params, format);
 	snd_pcm_hw_params_set_channels(pcm, params, nchannels);
 	snd_pcm_hw_params_set_rate_near(pcm, params, &val, 0);
 	snd_pcm_hw_params_set_periods(pcm, params, PERIOD, 0);
@@ -96,6 +111,23 @@ void snd_play(FILE *fp, size_t n) {
 
 #endif
 
+int wav_getfmt(int comptype, int bitdepth) {
+	switch (comptype) {
+	case 1:
+		bitdepth += bitdepth % 8;
+		switch (bitdepth) {
+		case 8:  return WAV_FMT_8;
+		case 16: return WAV_FMT_16;
+		case 24: return WAV_FMT_24;
+		case 32: return WAV_FMT_32;
+		default: return -1;
+		}
+	case 6: return WAV_FMT_A_LAW;
+	case 7: return WAV_FMT_MU_LAW;
+	default: return -1;
+	}
+}
+
 wavfile_t * wav_open(const char *fn) {
 	FILE *fp;
 	wavfile_t *wav = NULL;
@@ -105,20 +137,28 @@ wavfile_t * wav_open(const char *fn) {
 		wav = (wavfile_t*) malloc(sizeof(wavfile_t));
 		wav->stream = fp;
 		wav->size = header.nframes;
-		snd_set(header.format, header.nchannels, header.framerate);
+		int format = wav_getfmt(header.comptype, header.bitdepth);
+		if (format < 0) {
+			eputs("Unsupported PCM format");
+			fclose(wav->stream);
+			wav->stream = NULL;
+		} else snd_set(format, header.nchannels, header.framerate);
 	}
 	return wav;
 }
 
 void wav_close(wavfile_t *wav) {
-	fclose(wav->stream);
+	if (wav->stream) fclose(wav->stream);
 	free(wav);
 }
 
 void wav_play(const char *fn) {
 	wavfile_t *wav;
 	if ((wav = wav_open(fn))) {
-		snd_play(wav->stream, wav->size);
+		if (wav->stream)
+			snd_play(wav->stream, wav->size);
+		else
+			fprintf(stderr, "%s: Skipping file `%s'\n", __func__, fn);
 		wav_close(wav);
 	} else perror(__func__);
 }
